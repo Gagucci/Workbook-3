4e99fe669fc397e513c635e712f28202cad2415b
Kevin Gagante <KevinGagante@Gmail.com>
2025-04-22 11:55:29 -0700

    Made base for famous quotes exercise

---
+++ FamousQuotes/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Quotes[] quotes = {
+            new Quotes(1, "The greatest glory in living lies not in never falling, but in rising every time we fall."),
+            new Quotes(2, "The way to get started is to quit talking and begin doing."),
+            new Quotes(3, "Life is what happens when you're busy making other plans."),
+            new Quotes(4, "The future belongs to those who believe in the beauty of their dreams."),
+            new Quotes(5, "You only live once, but if you do it right, once is enough."),
+            new Quotes(6,"It ain't nothing to it but to go through it."),
+            new Quotes(7,"LALALALA"),
+            new  Quotes(8,"LALALALALALALALAL"),
+            new Quotes(9,"LAAALALALAL"),
+            new Quotes(10,"LALALALALALALALAL")
+        };
+    }
+}
---
+++ FamousQuotes/src/main/java/org/example/Quotes.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Quotes {
+    private int id;
+    private String quote;
+
+    public Quotes(int id, String quote) {
+        this.id = id;
+        this.quote = quote;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getQuote() {
+        return quote;
+    }
+}
