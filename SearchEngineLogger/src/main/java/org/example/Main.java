75871a9ab0f842288ddddd89b85d9984012c18c4
Kevin Gagante <KevinGagante@Gmail.com>
2025-04-24 11:21:01 -0700

    initial commit

---
+++ SearchEngineLogger/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please ");
+    }
+}
